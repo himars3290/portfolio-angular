d6cb093c80eea71cad8c50afd9e252dd017db641
rashmi <rashmi.manandhar7@gmail.com>
2019-10-02 11:50:37 -0400

    create gallery component and details component; add them to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GalleryComponent} from './gallery/gallery.component';
+import {DetailsComponent} from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GalleryComponent
+  },
+  {
+    path: 'project/:id',
+    component: DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
