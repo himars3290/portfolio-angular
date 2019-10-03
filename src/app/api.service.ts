c04e31dd14bcfd6c2c92de0d524c8d1c66f47410
rashmi <rashmi.manandhar7@gmail.com>
2019-10-02 20:50:03 -0400

    add images and api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  projects: any;
+
+  constructor(private _http: Http) {
+  }
+
+  getProjects() {
+    return this._http.get('/public/projects.json')
+      .pipe(
+        map(res => res.json())
+      );
+  }
+
+  getProject(id) {
+    return this._http.get('/public/projects.json')
+      .pipe(
+        map(res => {
+          return res.json()[id - 1];
+        })
+      );
+  }
+}
