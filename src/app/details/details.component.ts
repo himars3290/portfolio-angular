d0e9de31f9476196b8ac7a9e8cea19c2584134d6
rashmi <rashmi.manandhar7@gmail.com>
2019-10-03 11:51:33 -0400

    add animation for detail page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../api.service';
 import {ActivatedRoute} from '@angular/router';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-60px)', opacity: '0'}),
+        animate('800ms ease-out')
+      ])
+    ]),
+    trigger('slideInRight', [
+      state('true', style({transform: 'translateX(0)', opacity: '1'})),
+      transition('void => *', [
+        style({transform: 'translateX(150px)', opacity: '0'}),
+        animate('300ms 500ms ease-out')
+      ])
+    ]),
+    trigger('slider', [
+      state('true', style({transform: 'scaleY(0)'})),
+      transition('void => *', [
+        style({transform: 'scaleY(1)'}),
+        animate('300ms 800ms ease-in')
+      ])
+    ])
+  ]
 })
 export class DetailsComponent implements OnInit {
 
   project: any;
+  state:string = 'out';
+  state2: string = 'true';
 
   constructor(private api: ApiService, private router: ActivatedRoute) {
   }
