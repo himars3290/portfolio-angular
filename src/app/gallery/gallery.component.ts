067e2791d57499db98f35f8bf8633f6ac21205d7
rashmi <rashmi.manandhar7@gmail.com>
2019-10-02 20:48:52 -0400

    get the list of projects through service from json and display them

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../api.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  projects: any;
+
+  constructor(private api: ApiService) {
+  }
 
   ngOnInit() {
+    this.api.getProjects()
+      .subscribe(res => {
+        this.projects = res;
+      });
   }
 
 }
